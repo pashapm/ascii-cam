1e63ce8ce69e3ea395e7569523f892d4ddfa8974
jecklandin <jecklandin@c5e38b26-fae7-11de-91aa-85f76f42a988>
2010-01-16 23:57:27 +0000

    added remote-ex

--- ascii-cam/src/ru/jecklandin/asciicam/Preview.java
+++ ascii-cam/src/ru/jecklandin/asciicam/Preview.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 
 import android.content.Context;
 import android.hardware.Camera;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 
 
 class Preview extends SurfaceView implements SurfaceHolder.Callback {
+	static boolean camOpen;
     SurfaceHolder mHolder;
     Camera mCamera;
     AsciiCamera cam;
@@ -26,20 +28,23 @@ class Preview extends SurfaceView implements SurfaceHolder.Callback {
 
     public void surfaceCreated(SurfaceHolder holder) {
         try {
+        if (mCamera == null)	 {
+        	mCamera = Camera.open();
+        	Log.d("AAAAA", "$$$$$$$$$$$$$$$$$NULL");
+        }
            mCamera.setPreviewDisplay(holder);
         } catch (IOException exception) {
             mCamera.release();
             mCamera = null;
         }
     }
-
+   
     public void surfaceDestroyed(SurfaceHolder holder) {
         // Surface will be destroyed when we return, so stop the preview.
         // Because the CameraDevice object is not a shared resource, it's very
-        // important to release it when the activity is paused.
+        // important to release it when the activity is paused. 
         //mCamera.stopPreview();
         //mCamera.release();
-        mCamera = null;
     }
 
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
@@ -50,5 +55,11 @@ class Preview extends SurfaceView implements SurfaceHolder.Callback {
         mCamera.setParameters(parameters);
         mCamera.startPreview();
     }
+
+	public static void setcamOpen(boolean b) {
+		camOpen = b;
+		Log.d("%%%%%%", "CAM OPEN %%%%%%%%%%%%%%%%%^^^^^^");
+		
+	}
 
 }
